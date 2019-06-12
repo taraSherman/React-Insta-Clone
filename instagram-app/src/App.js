4caf2359a9429552b0618ba75898da4129afeed9
Tara Sherman <taradelin@gmail.com>
2019-06-12 19:39:53 -0400

    Folder structure laid out.

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
+import DummyData from './dummy-data';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+  constructor() {
+      super();
+      this.state = {};
+  };
+
+  render() {
+      return(
+          <div className="container">
+            test
+          </div>
+      )
+  }
 }
 
 export default App;
